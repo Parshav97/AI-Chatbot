916bb43862775ba7861ac0c1aa96c588b666b61f
Parshav97 <parshavnahta97@gmail.com>
2023-11-24 00:56:04 +0530

    twelth commit

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -4,19 +4,18 @@ const path = require('path')
 const { signup, login, logout, startConversation, reply } = require("../controllers/userController")
 const middleware = require("./middleware")
 
-const portNumber = process.env.PORT
 const domainName = process.env.DOMAIN
 
 
 userRouter
     .route("/signup")
     .post(signup)
-    .get((req, res)=> res.render(path.join(__dirname, '../public', 'signup.html'), {port:portNumber, domain:domainName} ))
+    .get((req, res)=> res.render(path.join(__dirname, '../public', 'signup.html'), {domain:domainName} ))
     
 userRouter
     .route("/login")
     .post(login)
-    .get((req, res)=> res.render(path.join(__dirname, '../public', 'login.html'), {port:portNumber, domain:domainName}))
+    .get((req, res)=> res.render(path.join(__dirname, '../public', 'login.html'), {domain:domainName}))
 
 userRouter
     .route("/logout")
@@ -32,7 +31,7 @@ userRouter
 
 userRouter
     .route("/dashboard")
-    .get(middleware, (req,res)=>res.render(path.join(__dirname, '../public', 'dashboard.html'), {port:portNumber, domain:domainName}))
+    .get(middleware, (req,res)=>res.render(path.join(__dirname, '../public', 'dashboard.html'), { domain:domainName}))
 
 
 module.exports = userRouter;
